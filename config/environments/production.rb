6b8e7af68e643d50e00508aa1839994e5d172c99
vrishab <vrishab2494@gmail.com>
2016-11-23 19:07:07 +0530

    added port to send via google

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   # for mailers
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+                                           :address              => "smtp.gmail.com",
+                                           :port                 => 587,
+                                           :user_name            => ENV['gmail_username'],
+                                           :password             => ENV['gmail_password'],
+                                           :authentication       => "plain",
+                                          :enable_starttls_auto => true
+                                          }
   config.action_mailer.default_url_options = { :host => "http:///limitless-reaches-25332.herokuapp.com" }
 end
