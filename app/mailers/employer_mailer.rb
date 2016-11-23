2b8efb18acafc5608c738e0c1397d0ff45ca422e
vrishab <vrishab2494@gmail.com>
2016-11-23 17:18:45 +0530

    validations are to be added

--- app/mailers/employer_mailer.rb
+++ app/mailers/employer_mailer.rb
@@ -1,7 +1,14 @@
 class EmployerMailer < ApplicationMailer
 	def verification_mail employer
 		@employer = employer
+		puts "============================="
+		puts employer.email
+		puts @employer.name
+		# @body[:employer] = @employer
+		# @body[:verification_code] = employer.eauth.auth_code
 		@verification_code = @employer.eauth.auth_code
+		puts "=========================================="
+		puts @verification_code
 		mail(to: @employer.email, subject: 'Verification code')
 	end
 end
